eb16c85aa24f6fd0ce3642ed1ccc8222544dd653
Rafay Aleem <rafay.aleem@pointclickcare.com>
2020-11-18 20:57:29 -0500

    Add bash script

--- code/pipeline/__main__.py
+++ code/pipeline/__main__.py
@@ -4,8 +4,10 @@ from pipeline.tasks import TfIdfSimilarityPipeline
 
 
 def main():
+    input_file = '/Users/aleemr/powerhouse/tf-idf-luigi-pipeline/documents.txt'
+
     print("Staring pipeline...")
-    luigi.build([TfIdfSimilarityPipeline()])
+    luigi.build([TfIdfSimilarityPipeline(input_path=input_file)])
 
 
 if __name__ == '__main__':
---
+++ run.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+PWD=$PWD
+
+echo "${PWD}"
+
+if [ $# != 1 ]; then
+  echo "Error: expecting exactly one argument (documents.txt), received [$#]: '$@'"
+  exit
+fi
+
+FULLPATH="${PWD}/${1}"
+
+echo "Full path for the documents file is ${FULLPATH}"
+
+echo "Will execute tasks in --local-scheduler mode so web server won't be accessible"
+#luigid &
+
+PYTHONPATH="${PWD}"/code luigi --module pipeline.tasks TfIdfSimilarityPipeline --inputpath "${FULLPATH}" --outputpath "${PWD}" --local-scheduler
